cb94597d22eb9e36cec2b8c695deac9f4dae58ef
SchwartzCode <jonbs98@gmail.com>
2021-06-01 16:11:07 -0500

    Code styling using feedback from flake8

--- astar/__init__.py
+++ astar/__init__.py
@@ -2,7 +2,7 @@
 """ generic A-Star path searching algorithm """
 
 from abc import ABCMeta, abstractmethod
-from heapq import heappush, heappop, heapify
+from heapq import heappush, heappop
 
 __author__ = "Julien Rialland"
 __copyright__ = "Copyright 2012-2017, J.Rialland"
@@ -10,7 +10,8 @@ __license__ = "BSD"
 __version__ = "0.93"
 __maintainer__ = __author__
 __email__ = ''.join(map(chr, [106, 117, 108, 105, 101, 110, 46, 114, 105,
-                              97, 108, 108, 97, 110, 100, 64, 103, 109, 97, 105, 108, 46, 99, 111, 109]))
+                              97, 108, 108, 97, 110, 100, 64, 103, 109, 97, 105,
+                              108, 46, 99, 111, 109]))
 __status__ = "Production"
 
 
@@ -45,19 +46,21 @@ class AStar:
 
     @abstractmethod
     def heuristic_cost_estimate(self, current, goal):
-        """Computes the estimated (rough) distance between a node and the goal, this method must be implemented in a subclass. The second parameter is always the goal."""
+        """Computes the estimated (rough) distance between a node and the goal, this method
+         must be implemented in a subclass. The second parameter is always the goal."""
         raise NotImplementedError
 
     @abstractmethod
     def distance_between(self, n1, n2):
-        """Gives the real distance between two adjacent nodes n1 and n2 (i.e n2 belongs to the list of n1's neighbors).
-           n2 is guaranteed to belong to the list returned by the call to neighbors(n1).
-           This method must be implemented in a subclass."""
+        """Gives the real distance between two adjacent nodes n1 and n2 (i.e n2 belongs to
+         the list of n1's neighbors). n2 is guaranteed to belong to the list returned by
+         the call to neighbors(n1). This method must be implemented in a subclass."""
         raise NotImplementedError
 
     @abstractmethod
     def neighbors(self, node):
-        """For a given node, returns (or yields) the list of its neighbors. this method must be implemented in a subclass"""
+        """For a given node, returns (or yields) the list of its neighbors. this method
+         must be implemented in a subclass"""
         raise NotImplementedError
 
     def is_goal_reached(self, current, goal):
@@ -110,7 +113,10 @@ class AStar:
         return None
 
 
-def find_path(start, goal, neighbors_fnct, reversePath=False, heuristic_cost_estimate_fnct=lambda a, b: Infinite, distance_between_fnct=lambda a, b: 1.0, is_goal_reached_fnct=lambda a, b: a == b):
+def find_path(start, goal, neighbors_fnct, reversePath=False,
+              heuristic_cost_estimate_fnct=lambda a, b: Infinite,
+              distance_between_fnct=lambda a, b: 1.0,
+              is_goal_reached_fnct=lambda a, b: a == b):
     """A non-class version of the path finding algorithm"""
     class FindPath(AStar):
 
