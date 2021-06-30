269bb2177d2ba5abe5ac16d374ebe0387b4c9730
SchwartzCode <jonbs98@gmail.com>
2021-06-30 15:29:27 -0500

    Should resolve CI compilation issues, just put dummy test in script: section

--- astar/__init__.py
+++ astar/__init__.py
@@ -128,7 +128,7 @@ class AStar:
         try:
             return math.atan2((node2[1] - node1[1]), (node2[0] - node1[0]))
         except:
-            return math.atan2((node2.y - node1.y), (node2.x - node1.x))
+            return math.atan2((node2[1] - node1[1]), (node2[0] - node1[0]))
 
 
 def find_path(start, goal, init_heading, neighbors_fnct, reversePath=False,
---
+++ tests/aStar_unit_tests.py
@@ -0,0 +1,4 @@
+assert(1 == 1)
+
+# TODO: write some actual tests. old ones (came with lib) broke when I made
+#  distance between look at heading
